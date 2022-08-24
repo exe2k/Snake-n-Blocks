0310f246655a68aadc1871375394394ee92e652e
exe2k <exe2k@mail.ru>
2022-08-25 02:36:07 +0300

    Particles Added

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class Finish : MonoBehaviour
 {
+
+    [SerializeField]ParticleSystem paricleSystem;
+
+    private void Start()
+    {
+        paricleSystem.Stop();
+    }
     private void OnTriggerEnter(Collider col)
     {
         Player player = null;
         player = col.GetComponentInParent<Player>();
         player?.Finish();
+
+        paricleSystem.Play();
     }
 }
