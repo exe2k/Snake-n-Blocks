86671cd9cae7797957fb6ec8f457e1635341e869
exe2k <exe2k@mail.ru>
2022-08-25 02:08:46 +0300

    update

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,8 +4,11 @@ public class Rotator : MonoBehaviour
 {
     public Vector3 AnglePerSecond;
 
+    [SerializeField]bool isDependOnPlayer = false;
+
     void Update()
     {
+        if (isDependOnPlayer && !Player.instance.isControlsOn) return;
         transform.rotation *= Quaternion.Euler(AnglePerSecond * Time.deltaTime);
     }
 }
